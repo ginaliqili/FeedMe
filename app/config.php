7e4e1d778ee9baad78a3f3f7349c77b34f5b00d7
Tommy Dean <tjdean@users.noreply.github.com>
2016-03-19 21:06:23 -0400

    Update config.php

--- app/config.php
+++ app/config.php
@@ -2,7 +2,7 @@
 
 // path constants
 define('SYSTEM_PATH', dirname(__FILE__)); # location of 'app' folder - don't change
-define('BASE_URL','http://localhost/apps/Project_3');
+define('BASE_URL','http://localhost/apps/Feed_Me');
 
 // database constants
 define('DB_HOST','127.0.0.1');
